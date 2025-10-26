7adb9083ffd10e9497c335462d291d5492ce65b5
斎木亮吾 <ryogo0825@gmail.com>
2025-10-26 20:18:00 +0900

    add_Google3Dtiles_19

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ window.onload = function() {
     
     // 4. 初期視点の設定 (500km 上空から日本を見る)
     viewer.camera.setView({
-        destination: Cesium.Cartesian3.fromDegrees(130.40, 33.59, 500000), 
+        destination: Cesium.Cartesian3.fromDegrees(130.45, 33.65, 800000), 
         orientation: {
             heading: 0, pitch: -1.4, roll: 0
         }
